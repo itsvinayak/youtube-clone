71b03f128772a409cea365658ef7e9e8d5749cbb
itsvinayak <itssvinayak@gmail.com>
2020-04-24 18:44:05 +0530

    Updates

---
+++ precache-manifest.4fb26535515971c59522c6cd234618b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7a59d9d0e94f6ff7988eb41135e6f6",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "5e45b47efc92854d9e5b",
+    "url": "/youtube-clone/static/css/main.a840ed81.chunk.css"
+  },
+  {
+    "revision": "01e63138813acfcda887",
+    "url": "/youtube-clone/static/js/2.a4e57b6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone/static/js/2.a4e57b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e45b47efc92854d9e5b",
+    "url": "/youtube-clone/static/js/main.2045ba36.chunk.js"
+  },
+  {
+    "revision": "d70cabcb6192dd484d18",
+    "url": "/youtube-clone/static/js/runtime-main.6d6bb464.js"
+  }
+]);
